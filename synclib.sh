d5b4700480bc2fedd309c952653bc0194dd5c196
notedit <notedit@gmail.com>
2016-11-11 22:42:12 +0800

    add video out renderer

--- synclib.sh
+++ synclib.sh
@@ -6,4 +6,4 @@
 cp /data/build/src/out_ios_libs/librtc_sdk_objc.a  .
 
 
-cp /data/build/src/out_ios_libs/WebRTC.framework/Headers/*  WebRTC/
+cp /data/build/src/webrtc/sdk/objc/Framework/Headers/WebRTC/*  WebRTC/
