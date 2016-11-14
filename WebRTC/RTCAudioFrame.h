da15ddb3b715d68b57344aab64983a7b9a7d67a3
notedit <notedit@gmail.com>
2016-11-14 12:44:56 +0800

    fix webrtc audio frame

--- WebRTC/RTCAudioFrame.h
+++ WebRTC/RTCAudioFrame.h
@@ -14,8 +14,17 @@ NS_ASSUME_NONNULL_BEGIN
 RTC_EXPORT
 @interface RTCAudioFrame : NSObject
 
+@property(nonatomic, readonly, nullable) const int16_t *data;
+@property(nonatomic, readonly) size_t samples_per_channel;
+@property(nonatomic, readonly) int sample_rate;
+@property(nonatomic, readonly) size_t channels;
+@property(nonatomic, readonly) uint32_t timestamp;
+
+@property(nonatomic, readonly) int  bits_per_sample;
+
 
 - (instancetype)init NS_UNAVAILABLE;
+
 
 @end
 
